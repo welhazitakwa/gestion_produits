cec517fc920d8c045bd8b1271e1f0176be10eaa7
welhazitakwa <welhazitakwa@gmail.com>
2024-06-12 13:48:12 +0200

    configuration du template dans les composants de base

--- src/app/admin/produits/produits.component.ts
+++ src/app/admin/produits/produits.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.component.html',
-  styleUrls: ['./produits.component.css']
+  styleUrls: ['./produits.component.css'],
 })
 export class ProduitsComponent {
-
-}
+  constructor(private router: Router) {}
+  logoImage = '/public/logo.jpg';
+ }
