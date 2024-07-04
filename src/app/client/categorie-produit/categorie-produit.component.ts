a0cdd02bd7ca8790705fecda990181402f584a16
welhazitakwa <welhazitakwa@gmail.com>
2024-07-04 22:08:58 +0200

    affichage des categories

--- src/app/client/categorie-produit/categorie-produit.component.ts
+++ src/app/client/categorie-produit/categorie-produit.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategorieService } from 'src/app/services/categorie.service';
 
 @Component({
   selector: 'app-categorie-produit',
   templateUrl: './categorie-produit.component.html',
-  styleUrls: ['./categorie-produit.component.css']
+  styleUrls: ['./categorie-produit.component.css'],
 })
-export class CategorieProduitComponent {
-
+export class CategorieProduitComponent implements OnInit {
+  getData: any[] = [];
+  constructor(
+    private categorieService: CategorieService
+  ) {}
+  ngOnInit(): void {
+    this.getCategoriesList();
+  }
+  getCategoriesList() {
+    this.categorieService.getCategories().subscribe({
+      next: (res) => {
+        // console.log(res);
+        this.getData = res;
+        console.log(this.getData);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
