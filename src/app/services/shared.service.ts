95792de47f72c8d4256439b47b93fb983be5b1de
welhazitakwa <welhazitakwa@gmail.com>
2024-07-05 16:45:57 +0200

    produits partie client

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-  intermd : any ;
-  idcat : any ;
-  constructor() { }
-  setMessage (data : any) {
+  intermd: any;
+  idcat: any;
+  nomcat: any;
+  constructor() {}
+  setMessage(data: any) {
     this.intermd = data;
   }
-  getMessage () {
-    return this.intermd ;
+  getMessage() {
+    return this.intermd;
   }
-  setIdcat (data : any) {
+  setIdcat(data: any) {
     this.idcat = data;
   }
-  getIdcat () {
+  getIdcat() {
     return this.idcat;
+  }
+  setNomcat(data: any) {
+    this.nomcat = data;
+  }
+  getNomcat() {
+    return this.nomcat;
   }
 }
